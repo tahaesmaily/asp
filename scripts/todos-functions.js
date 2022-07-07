bfa0ec79164454dd796e7a9c70c9fb4d342eff3e
0 <malakbalaiman@gmail.com>
2022-07-07 13:43:33 +0430

    Update todos-functions.js

--- scripts/todos-functions.js
+++ scripts/todos-functions.js
@@ -28,7 +28,7 @@ async function UPDATE(id,v) {
 
 const getSavedTodos = async() => {
   const todosJSON = localStorage.getItem('todos');
-  console.log(await SELECT().data)
+  console.log(await SELECT().data.data)
   var d =SELECT() ? SELECT() : (todosJSON ? JSON.parse(todosJSON) : []);
   
   try{
